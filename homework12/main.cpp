74cb52e0ff7211f05b74b9146ee1b0c37657ba67
Игорь Пуртов <jankoooy12@gmail.com>
2023-12-05 00:11:09 +0700

    Add homework12

--- homework12/main.cpp
+++ homework12/main.cpp
@@ -1,57 +1,44 @@
-#include <iostream>
-#include <list>
-#include <vector>
 #include <memory>
+#include <list>
+#include <iostream>
 
-template<typename T>
-class ContiguousAllocator {
+template<typename T, size_t pool_size_>
+class CustomAllocator {
 public:
     using value_type = T;
 
-    explicit ContiguousAllocator(std::size_t reserveSize) : capacity_(reserveSize), memory_(std::unique_ptr<T[]>(new T[capacity_])) {
-        nextIndex_ = 0;
-    }
+    template<typename U>
+    struct rebind {
+        using other = CustomAllocator<U, pool_size_>;
+    };
 
-    T* allocate(std::size_t n) {
-        if (n > available()) {
+    CustomAllocator() :
+            pool_(new T[pool_size_]),
+            cur_() {}
+
+    T *allocate(size_t n) {
+        if (cur_ + n > pool_size_)
             throw std::bad_alloc();
-        }
-        T* p = &memory_[nextIndex_];
-        nextIndex_ += n;
-        return p;
+        T *ptr = pool_ + cur_;
+        cur_ += n;
+        return (ptr);
     }
 
-    void deallocate(T* p, std::size_t n) {}
+    void deallocate(T *ptr, size_t n) {
+
+    }
 
 private:
-    std::size_t available() const {
-        return capacity_ - nextIndex_;
-    }
-
-    std::size_t capacity_;
-    std::unique_ptr<T[]> memory_;
-    std::size_t nextIndex_;
+    T *pool_;
+    size_t cur_;
 };
 
+
 int main() {
-    std::list<int, ContiguousAllocator<int>> myList(10);
+    std::list<int, CustomAllocator<int, 20>> test_list;
+    for (int i = 0; i < 15; i++)
+        test_list.push_back(i);
 
-    int value = 1;
-    for (auto& element : myList) {
-        element = value++;
-    }
-
-    std::cout << "Elements in the list: ";
-    for (const auto& element : myList) {
-        std::cout << element << " ";
-    }
-    std::cout << std::endl;
-
-    std::cout << "Memory addresses: ";
-    for (const auto& element : myList) {
-        std::cout << &element << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
+    for (const int &i: test_list)
+        std::cout << &i << std::endl;
 }
